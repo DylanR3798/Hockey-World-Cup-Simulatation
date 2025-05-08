5ad931eb50e1273b125e2c3cb2ead0d9170e0526
Dylan Reynolds <48947913+DylanR3798@users.noreply.github.com>
2025-05-08 11:29:59 +0100

    Delete IND.py

--- IND.py
+++
@@ -1,7 +0,0 @@
-Singh = Goalkeeper('Harmanpreet Singh', 79, 72, 77, 74)
-Tirkey = Outfield_Player('Dipsan Tirkey', 'DF', 61, 79, 75, 78, 68, 70)
-Sharma = Outfield_Player('Nilakanta Sharma', 'MF', 72, 68, 72, 79, 78, 74)
-Qureshi = Outfield_Player('Armaan Qureshi', 'MF', 76, 68, 77, 72, 75, 73)
-Yousuf = Outfield_Player('Affan Yousuf', 'FW', 80, 70, 70, 74, 76, 73)
-
-IND = Team('India', Singh, Tirkey, Sharma, Qureshi, Yousuf)
