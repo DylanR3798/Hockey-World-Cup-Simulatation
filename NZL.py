859926626d75d94965c8abf9bb0269d097ef0e7b
Dylan Reynolds <48947913+DylanR3798@users.noreply.github.com>
2025-05-08 11:31:07 +0100

    Delete NZL.py

--- NZL.py
+++
@@ -1,7 +0,0 @@
-Manchester = Goalkeeper('Devon Manchester', 74, 78, 71, 70)
-Hilton = Outfield_Player('Blair Hilton', 'DF', 56, 78, 71, 70, 73, 72)
-Archibald = Outfield_Player('Ryan Archibald', 'MF', 79, 65, 78, 77, 75, 72)
-Child = Outfield_Player('Simon Child', 'MF', 67, 72, 75, 73, 68, 76)
-Shaw = Outfield_Player('Bradley Shaw', 'FW', 76, 62, 77, 75, 74, 79)
-
-NZL = Team('New Zealand', Manchester, Hilton, Archibald, Child, Shaw)
