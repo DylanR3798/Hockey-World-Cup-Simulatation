71be9e1c59322863433c5c75814322a593e7f875
Dylan Reynolds <48947913+DylanR3798@users.noreply.github.com>
2025-05-08 11:27:45 +0100

    Delete AUS.py

--- AUS.py
+++
@@ -1,7 +0,0 @@
-Charter = Goalkeeper('Andrew Charter', 84, 80, 75, 78)
-Dawson = Outfield_Player('Mattew Dawson', 'DF', 74, 86, 80, 79, 80, 81)
-Wickham = Outfield_Player('Tom Wickham', 'MF', 79, 78, 80, 82, 80, 81)
-Edwards = Outfield_Player('Jeremy Edwards', 'MF', 80, 81, 76, 82, 80, 75)
-Craig = Outfield_Player('Tom Craig', 'FW', 95, 65, 81, 82, 77, 80)
-
-AUS = Team('Australia', Charter, Dawson, Wickham, Edwards, Craig)
