655385272a073005c9a6cddc8c0ff69f74321249
Dylan Reynolds <48947913+DylanR3798@users.noreply.github.com>
2025-05-08 11:30:31 +0100

    Delete MAL.py

--- MAL.py
+++
@@ -1,7 +0,0 @@
-Othman = Goalkeeper('Hafizuddin Othman', 74, 72, 68, 70)
-Rahim = Outfield_Player('Razie Rahim', 'DF', 70, 71, 71, 72, 69, 73)
-Hassan = Outfield_Player('Azi Hassan', 'MF', 77, 73, 76, 71, 74, 72)
-Saari = Outfield_Player('Fitri Saari', 'MF', 76, 71, 67, 68, 70, 72)
-Saabah = Outfield_Player('Shahril Saabah', 'FW', 71, 73, 75, 70, 76, 73)
-
-MAL = Team('Malaysia', Othman, Rahim, Hassan, Saari, Saabah)
