db8cb2ec7e959bd565ad1adc7ae2e9c88d755661
Dylan Reynolds <48947913+DylanR3798@users.noreply.github.com>
2025-05-08 11:29:20 +0100

    Delete GER.py

--- GER.py
+++
@@ -1,7 +0,0 @@
-Jacobi = Goalkeeper('Niclas Jacobi', 80, 73, 78, 77)
-Butt = Outfield_Player('Linus Butt', 'DF', 60, 87, 76, 75, 70, 75)
-Tompertz = Outfield_Player('Moritz Tompertz', 'MF', 70, 69, 73, 80, 77, 73)
-Herzbruch = Outfield_Player('Timm Herzbruch', 'MF', 81, 73, 72, 74, 75, 73)
-Grambusch = Outfield_Player('Tom Grambusch', 'FW', 78, 68, 72, 73, 72, 74)
-
-GER = Team('Germany', Jacobi, Butt, Tompertz, Herzbruch, Grambusch)
