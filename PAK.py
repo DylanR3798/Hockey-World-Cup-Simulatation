f38eb0d462b8735d8af9fe594c2985add90d9303
Dylan Reynolds <48947913+DylanR3798@users.noreply.github.com>
2025-05-08 11:31:36 +0100

    Delete PAK.py

--- PAK.py
+++
@@ -1,7 +0,0 @@
-Ali = Goalkeeper('Amjad Ali', 66, 71, 67, 68)
-Mahmood = Outfield_Player('Abu Mahmood', 'DF', 63, 73, 70, 78, 67, 63)
-Shaked = Outfield_Player('Ammad Shaked', 'MF', 79, 69, 69, 66, 69, 74)
-Ashfaq = Outfield_Player('Nawaz Ashfaq', 'MF', 68, 70, 69, 74, 63, 79)
-Abbas = Outfield_Player('Tasawar Abbas', 'FW', 79, 63, 68, 77, 69, 77)
-
-PAK = Team('Pakistan', Ali, Mahmood, Shaked, Ashfaq, Abbas)
